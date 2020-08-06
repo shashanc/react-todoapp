5a53034cdd5d73806134bacec867cec7e6ede93c
shashanc <shanksharma19@gmail.com>
2020-08-06 13:19:27 +0530

    Updates

---
+++ precache-manifest.b7da0b814df1a62b27ab8957c724661d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c219408f92751bdccf0b4395661410",
+    "url": "/react-todoapp/index.html"
+  },
+  {
+    "revision": "dfdcb58fb469f4760235",
+    "url": "/react-todoapp/static/css/main.af1ff74d.chunk.css"
+  },
+  {
+    "revision": "3d93f50f34f48da7c58b",
+    "url": "/react-todoapp/static/js/2.89ec83f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todoapp/static/js/2.89ec83f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfdcb58fb469f4760235",
+    "url": "/react-todoapp/static/js/main.3370681b.chunk.js"
+  },
+  {
+    "revision": "0e94ec605959bf8544b3",
+    "url": "/react-todoapp/static/js/runtime-main.92a62adf.js"
+  }
+]);
